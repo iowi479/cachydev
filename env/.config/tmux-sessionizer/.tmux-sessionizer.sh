5ae5ef8188a293828a1b1f1c42e29662549de4d1
Iowi479 <simon@alle-schindlers.de>
2025-10-24 18:26:28 +0200

    rewrite tmux-sessionizer

--- env/.config/tmux-sessionizer/.tmux-sessionizer.sh
+++ env/.config/tmux-sessionizer/.tmux-sessionizer.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-echo "Skript running"
-if [ "$(pwd)" = "$HOME/personal" ]; then
-    clear
- #return   exit
-fi
+# Tmux Sessionizer Script
+#
+# This is a hydration script for tmux sessions.
+# Here you can define which windows should be created
+# and what commands should be run in each window.
 
-tmux new-window -dn run
-#clear
+
+tmux new-window -d -k -n 'dev' -t 2
+tmux new-window -d -k -n 'nvim' -t 1 nvim .
 
 tmux select-window -t 1
-nvim .
-#clear
+
 
