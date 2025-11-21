46bd5581a0c62136ca8e1d37a98039c19c25c9c8
Iowi479 <simon@alle-schindlers.de>
2025-11-21 14:30:19 +0100

    small updates

--- env/.config/nvim/init.lua
+++ env/.config/nvim/init.lua
@@ -1,10 +1,13 @@
 require("settings")
 require("remap")
 
+-- Workaround for trouble
+vim.hl = vim.highlight
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
